00e6b4d49cbfe79bf17a1193ff5b0a3c197797e1
Rui Meng <bennymeng1226@gmail.com>
2024-12-30 20:53:58 -0400

    finish the first 2 method

--- proj0/game2048/Model.java
+++ proj0/game2048/Model.java
@@ -137,7 +137,13 @@ public class Model extends Observable {
      *  Empty spaces are stored as null.
      * */
     public static boolean emptySpaceExists(Board b) {
-        // TODO: Fill in this function.
+        for(int i = 0; i < 4; i++){
+            for(int j = 0; j < 4; j++){
+                if(b.tile(i, j) == null){
+                    return true;
+                }
+            }
+        }
         return false;
     }
 
@@ -147,7 +153,13 @@ public class Model extends Observable {
      * given a Tile object t, we get its value with t.value().
      */
     public static boolean maxTileExists(Board b) {
-        // TODO: Fill in this function.
+        for(int i = 0; i < 4; i++){
+            for(int j = 0; j < 4; j++){
+                if(b.tile(i, j) != null && b.tile(i, j).value() == MAX_PIECE){
+                    return true;
+                }
+            }
+        }
         return false;
     }
 
@@ -158,7 +170,17 @@ public class Model extends Observable {
      * 2. There are two adjacent tiles with the same value.
      */
     public static boolean atLeastOneMoveExists(Board b) {
-        // TODO: Fill in this function.
+        for(int i = 0; i < 4; i++){
+            for(int j = 0; j < 4; j++){
+                if(b.tile(i, j) == null){
+                    return true;
+                }
+                if(i < 3 &&b.tile(i, j).value() == b.tile(i + 1, j).value()
+                        || j < 3 && b.tile(i, j).value() == b.tile(i, j + 1).value()){
+                    return true;
+                }
+            }
+        }
         return false;
     }
 
